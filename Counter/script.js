d2d1f45b79755aff4d069a40e559bfcfd020a7e5
Finn <mailtorufina@gmail.com>
2023-04-17 16:06:13 +0100

    add event listener for buttons

---
+++ Counter/script.js
@@ -0,0 +1,24 @@
+const button = document.querySelector(".buttons");
+
+let counter = 0;
+
+const add = () => {
+  counter++;
+  console.log(counter);
+};
+
+const clickHandler = (event) => {
+  console.log(event.target);
+  const clicked = event.target;
+  console.log(clicked.classList);
+  //   console.log(clicked, clicked.class, clicked);
+
+  if (clicked.classList.contains("btn-add")) {
+    console.log("Positive click!");
+    add();
+  } else if (clicked.classList.contains("btn-sub")) {
+    console.log("minus");
+  }
+};
+
+button.addEventListener("click", clickHandler);
