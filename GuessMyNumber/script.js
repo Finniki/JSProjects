053da8d084fde279d39f843e54d754406d704382
Finn <mailtorufina@gmail.com>
2023-04-24 20:39:20 +0100

    add js selectors to manipulate the DOM

---
+++ GuessMyNumber/script.js
@@ -0,0 +1,10 @@
+const highScore = document.querySelector(".high-score");
+const gameScore = document.querySelector(".game-score");
+const guess = document.querySelector(".guess");
+const feedback = document.querySelector(".feedback-div");
+
+// Input a number
+//check the number
+// give feedback; too high, too low, correct
+//adjust the score/
+//at tjhe end of the game, adjust the highsscore.
