e6eff13bc47047d28235dc384ee1324304c1074b
Finn <mailtorufina@gmail.com>
2023-04-26 20:18:58 +0100

    add clickHandler function

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,10 +1,15 @@
 const highScore = document.querySelector(".high-score");
 const gameScore = document.querySelector(".game-score");
-const guess = document.querySelector(".guess");
+const guessBox = document.querySelector(".guess");
 const feedback = document.querySelector(".feedback-div");
 
 // Input a number
+const clickHandler = (event) => {
+  const guess = event.target.value;
+  console.log(guess);
+};
 //check the number
+// const checkGuess = () = {}
 // give feedback; too high, too low, correct
 //adjust the score/
 //at tjhe end of the game, adjust the highsscore.
