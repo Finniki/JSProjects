524831856d4060059c535906ceb264488699c559
Finn <mailtorufina@gmail.com>
2023-04-26 20:32:13 +0100

    retrieve user guess

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,13 +1,14 @@
 const highScore = document.querySelector(".high-score");
 const gameScore = document.querySelector(".game-score");
 const guessBox = document.querySelector(".guess");
+const guessBtn = document.querySelector(".btn");
 const feedback = document.querySelector(".feedback-div");
 
 // Input a number
 const clickHandler = (event) => {
-  const guess = event.target.value;
-  console.log(guess);
+  const guess = guessBox.value;
 };
+guessBtn.addEventListener("click", clickHandler);
 //check the number
 // const checkGuess = () = {}
 // give feedback; too high, too low, correct
