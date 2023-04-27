a45d41d3e02137819045697d271aa5d86d467b67
Finn <mailtorufina@gmail.com>
2023-04-27 21:37:04 +0100

    refactor error handler code

--- Counter/script.js
+++ Counter/script.js
@@ -16,15 +16,12 @@ const clickHandler = (event) => {
   console.log(event.target);
   const clicked = event.target;
   console.log(clicked.classList);
-  //   console.log(clicked, clicked.class, clicked);
   clicked.classList.contains("increment-button")
     ? incrementCounter()
     : decrementCounter();
 
   count.innerHTML = counter;
 };
-if (button && count) {
-  button.addEventListener("click", clickHandler);
-} else {
-  console.error("Could not find button or count element");
-}
+button && count
+  ? button.addEventListener("click", clickHandler)
+  : console.error("Could not find button or count element");
