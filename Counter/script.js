197f38c1ed4d516782f7e73c6008cb298fa26308
Finn <mailtorufina@gmail.com>
2023-04-27 21:35:01 +0100

    refactor if statements

--- Counter/script.js
+++ Counter/script.js
@@ -17,13 +17,10 @@ const clickHandler = (event) => {
   const clicked = event.target;
   console.log(clicked.classList);
   //   console.log(clicked, clicked.class, clicked);
-  if (clicked.classList.contains("btn-add")) {
-    console.log("Positive click!");
-    incrementCounter();
-  } else if (clicked.classList.contains("btn-sub")) {
-    console.log("minus");
-    decrementCounter();
-  }
+  clicked.classList.contains("increment-button")
+    ? incrementCounter()
+    : decrementCounter();
+
   count.innerHTML = counter;
 };
 if (button && count) {
